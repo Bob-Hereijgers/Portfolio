ff7ad16127e4138cf32530a6a596bf1e48a737b9
Bob-hereijgers <bob.hereijgers@gmail.com>
2026-05-13 17:40:21 +0200

    navbar update

--- src/components/Base Components/Navbar/Navbar.tsx
+++ src/components/Base Components/Navbar/Navbar.tsx
@@ -3,49 +3,37 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavLink } from "react-router-dom";
-import { useEffect, useState } from "react";
-import NextMCU from "./Movie";
+import "./navbar.css"; // Import the custom styles
 
-const Bar = () => {
-  const [movie, setMovie] = useState<NextMCU>();
-  
-  useEffect(() => {
-    const fetchFunction = async () => {
-      let result = await fetch(
-        `https://www.whenisthenextmcufilm.com/api`
-      );
-      let json: NextMCU = await result.json();
+interface BarProps {
+  // Add props here if needed
+}
 
-      setMovie(json);
-    };
-    fetchFunction();
-  });
-
+const Bar: React.FC<BarProps> = () => {
   return (
-    <div className="Wrapper"> 
-    <Navbar>
-      <Container>
-        <Navbar.Brand>My Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="NavBar">
-            <NavLink className="NavText" to="/">
-              Home
-            </NavLink>
-            <NavLink className="NavText" to="ExerciseGallery">
-              Exercise Gallery
-            </NavLink>
-            <NavLink className="NavText" to="Contact">
-              Contact Us
-            </NavLink>
-            <NavLink className="NavText" to="Favorites">
-              Favorites
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <p className="mcuText">{movie?.title} comes out in: {movie?.days_until} days</p>
+    <div className="wrapper">
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand>My Portfolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav-bar">
+              <NavLink className="nav-text" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-text" to="ExerciseGallery">
+                Exercise Gallery
+              </NavLink>
+              <NavLink className="nav-text" to="Contact">
+                Contact Us
+              </NavLink>
+              <NavLink className="nav-text" to="Favorites">
+                Favorites
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
